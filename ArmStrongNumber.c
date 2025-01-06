2088c8ab81ea2bbec8ca816e492f4ff1207884df
Rithik Kumar <rithikarchadefacilator@gmail.com>
2025-01-06 13:09:38 +0530

    Update ArmStrongNumber.c

--- ArmStrongNumber.c
+++ ArmStrongNumber.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
 int main(){
-    int n, r, temp, sum = 0;
+    int num, sum = 0, r, count = 0;
     printf("Enter the number");
     scanf("%d", &n);
-    temp = n;
+    num = count;
     while(n > 0){
         r = n % 10;
         sum = sum + r * r * r;
-        n = n / 10;
+        num = num / 10;
     }
-    if(temp == sum)
+    if(count == sum)
     printf("Armstrong number");
     else
     printf("Not armstrong number");
